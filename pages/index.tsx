710856ce4f4ac78b0aed7c0c453bffae698dcb2a
Min-Ho-Lim <paka7245@gmail.com>
2022-11-12 13:59:34 -0500

    fix(#1): fixed starfall effect out of bound issue and changed some css of starfall effect

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,8 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
+const arrayOfTen = Array(10).fill(0);
+
 export default function Home() {
   return (
     <div>
@@ -11,7 +13,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <h1 className={styles.title}>
+        <div className={styles.mainPage}>
           <a className={styles.topLeftText}>MHL</a>
           <div className={styles.titlegroup}>
             <a className={styles.titletext}>👋 Hello, my name is</a>
@@ -20,57 +22,12 @@ export default function Home() {
           <p className={styles.subtitle}>
             I'm a <b>software engineer 👨‍💻</b> based in <b>the United States</b>
           </p>
-
-          <span className={styles.span}></span>
-          <span className={styles.span}></span>
-          <span className={styles.span}></span>
-          <span className={styles.span}></span>
-          <span className={styles.span}></span>
-          <span className={styles.span}></span>
-          <span className={styles.span}></span>
-        </h1>
-
-        <p className={styles.description}>
-          This is my portfolio website. I am a software engineer.
-        </p>
-        <div className={styles.grid}>
-          {/* <Link href="/about">
-                        <a className={styles.card}>
-                            <h2>About &rarr;</h2>
-                            <p>Learn about me</p>
-                        </a>
-                    </Link>
-                    <Link href="/blog">
-                        <a className={styles.card}>
-                            <h2>Blog &rarr;</h2>
-                            <p>Read my blog</p>
-                        </a>
-                    </Link>
-                    <Link href="/projects">
-                        <a className={styles.card}>
-                            <h2>Projects &rarr;</h2>
-                            <p>Check out my projects</p>
-                        </a>
-                    </Link>
-                    <Link href="/contact">
-                        <a className={styles.card}>
-                            <h2>Contact &rarr;</h2>
-                            <p>Get in touch with me</p>
-                        </a>
-                    </Link> */}
         </div>
+        {/* Create 10 of spans */}
+        {arrayOfTen.map((_, i) => (
+          <span key={i} className={styles.span} />
+        ))}
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
   );
 }
