004d68b06b393b24d27bb8b910f59510719c1b37
Min-Ho-Lim <paka7245@gmail.com>
2022-11-12 16:52:01 -0500

    feat: created the icon bar with github, linkedin, mail icon and mobile friendly as well

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import ReactTooltip from "react-tooltip";
 
 const arrayOfTen = Array(10).fill(0);
 
@@ -15,6 +16,40 @@ export default function Home() {
       <main>
         <div className={styles.mainPage}>
           <a className={styles.topLeftText}>MHL</a>
+          <div className={styles.iconBarContainer}>
+            <div className={styles.iconBarStickBar} />
+            <ReactTooltip place="right" type="dark" effect="float" />
+            <a href="https://github.com/Min-Ho-Lim">
+              <Image
+                src="/github-icon.svg"
+                alt="Github Icon"
+                width={35}
+                height={35}
+                data-tip="My Github URL"
+              />
+            </a>
+            <a href="https://www.linkedin.com/in/min-ho-lim/">
+              <Image
+                src="/linkedin-icon.svg"
+                height={35}
+                width={35}
+                alt={"LinkedIn Icon"}
+                className={styles.icon}
+                data-tip="My LinkedIn Profile"
+              />
+            </a>
+            <a href="mailto:minholim@outlook.com">
+              <Image
+                src="/mail-icon.svg"
+                height={35}
+                width={35}
+                alt={"Mail Icon"}
+                className={styles.icon}
+                data-tip="My Email Address"
+              />
+            </a>
+            <div className={styles.iconBarStickBar} />
+          </div>
           <div className={styles.titlegroup}>
             <a className={styles.titletext}>👋 Hello, my name is</a>
             <a className={styles.titletextgradient}> Min Ho Lim</a>
