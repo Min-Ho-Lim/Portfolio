de395a77342cb597b72cb0d9ed1f9ae2a1a1bf12
Min-Ho-Lim <paka7245@gmail.com>
2022-11-16 20:51:21 -0500

    feat(#13): create a profilepage component and added wave svg to connect smoothly

---
+++ components/profilePage.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+import styles from "../styles/ProfilePage.module.css";
+import ReactTooltip from "react-tooltip";
+
+export default function ProfilePage() {
+  return <div className={styles.background}></div>;
+}
