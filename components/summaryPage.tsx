a0b741e32e2f0059eb1fc0ed90a2f48a61637a09
Min-Ho-Lim <paka7245@gmail.com>
2022-11-29 20:22:58 -0500

    feat(#20): created the contact me section

--- components/summaryPage.tsx
+++ components/summaryPage.tsx
@@ -1,9 +1,45 @@
 import Image from "next/image";
 import styles from "../styles/SummaryPage.module.css";
+import inputStyles from "../styles/InputField.module.css";
 import ReactTooltip from "react-tooltip";
 import IconBar from "./iconBar";
+import { useState } from "react";
+
+const InputField = ({
+  label,
+  value,
+  onChange,
+  placeholder,
+}: {
+  label: string;
+  value: string;
+  onChange: undefined;
+  placeholder: string;
+}) => {
+  return (
+    <div className={inputStyles.field}>
+      <div className={inputStyles.field_v2}>
+        <label htmlFor="first-name" className={inputStyles["ha-screen-reader"]}>
+          {label}
+        </label>
+        <input
+          id={label}
+          className={inputStyles.field__input}
+          placeholder={placeholder}
+          value={value}
+          onChange={onChange}
+        ></input>
+        <span className={inputStyles["field__label-wrap"]} aria-hidden="true">
+          <span className={inputStyles.field__label}>{label}</span>
+        </span>
+      </div>
+    </div>
+  );
+};
 
 export default function SummaryPage() {
+  const [email, setEmail] = useState("");
+
   return (
     <div className={styles.SummaryPageContainer}>
       <div className={styles.summaryContainer}>
@@ -30,6 +66,42 @@ export default function SummaryPage() {
             Fill out the form to send me an email and I will get back to you as
             soon as possible.
           </div>
+          {/* create contactInput*/}
+
+          <div className={styles.contactInputContainer}>
+            <InputField
+              label="Your Email"
+              value={email}
+              onChange={(e) => {
+                setEmail(e.value);
+              }}
+              placeholder="eg. minholim@outlook.com"
+            />{" "}
+            <InputField
+              label="Title"
+              value=""
+              onChange={() => {}}
+              placeholder="eg. What an Awesome Website!"
+            />
+            <InputField
+              label="Context"
+              value=""
+              onChange={() => {}}
+              placeholder="eg. Do you want to build a snowman?"
+            />
+          </div>
+
+          <span className={styles.contactSendButton}>
+            <Image
+              src="/sent-icon.svg"
+              alt="Sent Icon"
+              width={25}
+              height={25}
+              data-tip="Send the Email"
+              className={styles.contactSendImage}
+            />
+            <a className={styles.contactSendButtonText}>Send</a>
+          </span>
         </div>
       </div>
     </div>
